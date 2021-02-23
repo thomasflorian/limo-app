af6286ff32550f95990a2d9d1ea307a085ddcbd7
gld-2000 <71797899+gld-2000@users.noreply.github.com>
2021-02-23 02:45:03 -0600

    Update app-routing.module.ts

--- LimoApp/src/app/app-routing.module.ts
+++ LimoApp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoadingPage } from './loading/loading.page';
 
 const routes: Routes = [];
 
