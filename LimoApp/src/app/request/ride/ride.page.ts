b7dd150f6cbba40819b3a1c79c0d7117ebf35f62
Thomas Florian <thomas.florian@marquette.edu>
2021-04-02 17:57:18 -0500

    added map api

--- LimoApp/src/app/request/ride/ride.page.ts
+++ LimoApp/src/app/request/ride/ride.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { GoogleMaps } from '@ionic-native/google-maps';
 import { IonRouterOutlet, MenuController } from '@ionic/angular';
 
 @Component({
@@ -9,6 +10,8 @@ import { IonRouterOutlet, MenuController } from '@ionic/angular';
 })
 export class RidePage implements OnInit {
 
+  @ViewChild('map', {static:false}) mapElement: ElementRef;
+  map: google.maps.Map;
   constructor(private router: Router, private routerOutlet: IonRouterOutlet, private menu: MenuController) { }
 
   ngOnInit() {
@@ -16,8 +19,24 @@ export class RidePage implements OnInit {
     this.menu.swipeGesture(false);
   }
 
+  ionViewWillEnter(){
+    this.loadMap();
+  }
+
   cancel() {
     this.router.navigate([".."]);
+  }
+
+  loadMap(){
+    let latLng = new google.maps.LatLng(43.040119796976896, -87.93435173870222);
+    let mapOptions : google.maps.MapOptions = {
+      center: latLng,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
 }
