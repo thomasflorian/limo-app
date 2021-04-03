3b779fdaedc765f22499df8843e287a9622f5b95
Thomas Florian <thomas.florian@marquette.edu>
2021-04-02 21:34:28 -0500

    fixed ride cancel navigation animation going forward instead of back

--- LimoApp/src/app/request/ride/ride.page.ts
+++ LimoApp/src/app/request/ride/ride.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '../../interfaces/location';
-import { IonRouterOutlet, MenuController } from '@ionic/angular';
+import { IonRouterOutlet, MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ride',
@@ -12,25 +12,31 @@ export class RidePage implements OnInit {
 
   selectedLocation: Location;
 
+  // ViewChild selects the div element with id:"map" to load the map into.
   @ViewChild('map', {static:false}) mapElement: ElementRef;
   map: google.maps.Map;
-  constructor(private router: Router, private route: ActivatedRoute, private routerOutlet: IonRouterOutlet, private menu: MenuController) { }
+  constructor(private navCtrl: NavController, private router: Router, private route: ActivatedRoute, private routerOutlet: IonRouterOutlet, private menu: MenuController) { }
 
+  // Runs when page first loads.
   ngOnInit() {
     this.routerOutlet.swipeGesture = false;
     this.menu.swipeGesture(false);
     this.selectedLocation = this.router.getCurrentNavigation().extras.state.loc;
   }
 
+  // Runs everytime page is loaded.
   ionViewWillEnter(){
     this.loadMap();
   }
 
+  // Runs when cancel buttin is clicked.
   cancel() {
-    this.router.navigate([".."]);
+    this.navCtrl.pop();
   }
 
+  // Loads the google map api
   loadMap(){ 
+    // Set map options
     let latLng = new google.maps.LatLng(this.selectedLocation.gps[0], this.selectedLocation.gps[1]);
     let mapOptions : google.maps.MapOptions = {
       center: latLng,
@@ -38,7 +44,6 @@ export class RidePage implements OnInit {
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true
     };
-
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
